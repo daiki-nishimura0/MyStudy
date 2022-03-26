3e6f82d52c8bc79f4fee8c515628167d2ccb56d5
daiki nishimura0 <nishimura_dai@deltanet.co.jp>
2022-03-27 08:10:22 +0900

    fix: メインなし

--- app/src/main/java/com/example/mystudy/MainActivity.kt
+++ app/src/main/java/com/example/mystudy/MainActivity.kt
@@ -2,29 +2,10 @@ package com.example.mystudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        main()
-        Log.d("increment()","test")
-    }
-
-    fun main() {
-        val close = increment()
-
-        println(close())
-        println(close())
-    }
-// クロージャ
-    private fun increment(): ()->Int{
-        var x = 0
-        return {
-            x++
-            x
-        }
     }
 }
