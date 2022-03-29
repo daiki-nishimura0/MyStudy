8d6d462eaec48fa077aaf85bccc10c5483fa9fcd
daiki nishimura0 <nishimura_dai@deltanet.co.jp>
2022-03-29 23:18:22 +0900

    feat: ラムダの学習

--- app/src/main/java/com/example/mystudy/MainActivity.kt
+++ app/src/main/java/com/example/mystudy/MainActivity.kt
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    // ラムダの学習
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    println(lambda(2))
+
+    }
+    val lambda: (Int) -> Int = { i: Int ->
+        i + i
     }
 }
