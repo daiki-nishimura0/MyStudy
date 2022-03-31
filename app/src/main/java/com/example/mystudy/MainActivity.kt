9e3e6aadf564367da1e7b5e934d7e35af398b78e
daiki nishimura0 <nishimura_dai@deltanet.co.jp>
2022-03-31 18:44:59 +0900

    feat: 例外の学習　try-catch

--- app/src/main/java/com/example/mystudy/MainActivity.kt
+++ app/src/main/java/com/example/mystudy/MainActivity.kt
@@ -2,16 +2,23 @@ package com.example.mystudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
     // 例外を意図的に起こす
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        //  例外処理
         val text = "例外"
-        val number = text.toInt()
-
-        println(number)
+        try {
+            // 成功時の処理
+            val number = text.toInt()
+            println(number)
+            // String型の変数textにIntに変換できるものがない為以下のExptionが発生
+        } catch (e: NumberFormatException) {
+            // 例外発生時の処理
+            println("「${text}」という文字は数字に変換できない")
+        }
     }
 }
