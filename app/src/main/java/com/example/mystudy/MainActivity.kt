a96a486031d14b7e9e9e1735b9e3f7771e0e28c4
daiki nishimura0 <nishimura_dai@deltanet.co.jp>
2022-03-31 18:28:54 +0900

    feat: 例外の学習

--- app/src/main/java/com/example/mystudy/MainActivity.kt
+++ app/src/main/java/com/example/mystudy/MainActivity.kt
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    // 例外を意図的に起こす
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text = "例外"
+        val number = text.toInt()
+
+        println(number)
     }
 }
