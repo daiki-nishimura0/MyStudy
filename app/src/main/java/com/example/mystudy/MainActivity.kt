2aff2692d108785fc546b21e21a191d7cee832f3
daiki nishimura0 <nishimura_dai@deltanet.co.jp>
2022-04-12 23:04:09 +0900

    高階関数　オリジナルカレーを作る関数

--- app/src/main/java/com/example/mystudy/MainActivity.kt
+++ app/src/main/java/com/example/mystudy/MainActivity.kt
@@ -8,20 +8,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val p = ::calculatePlus
-        val m = ::calculateMinus
-        printRandomValuesCaluclation(p)
-        printRandomValuesCaluclation(m)
+//        val p = ::calculatePlus
+//        val m = ::calculateMinus
+//        printRandomValuesCaluclation(p)
+//        printRandomValuesCaluclation(m)
+        println(makeCook("鶏むね"))
+        println(makeSome("筋肉") { it + "カレー" })
+
     }
-    // 足し算をする関数の定義
-    fun calculatePlus(x: Double, y: Double) = x + y
-    // 引き算をする関数の定義
-    fun calculateMinus(x: Double, y: Double) = x - y
-    // 関数をうけとって、実行する関数
-    fun printRandomValuesCaluclation(calculator: (Double,Double) -> Double) {
-        val x = Math.random()
-        val y = Math.random()
-        val result = calculator(x, y)
-        println("計算結果${result}")
+//    // 足し算をする関数の定義
+//    fun calculatePlus(x: Double, y: Double) = x + y
+//    // 引き算をする関数の定義
+//    fun calculateMinus(x: Double, y: Double) = x - y
+//    // 関数をうけとって、実行する関数
+//    fun printRandomValuesCaluclation(calculator: (Double,Double) -> Double) {
+//        val x = Math.random()
+//        val y = Math.random()
+//        val result = calculator(x, y)
+//        println("計算結果${result}")
+//    }
+    // 抽象化した関数　汎用性のあるものを定義
+    fun makeCook(origin:String): String = "${origin}カレー"
+
+    fun makeSome(origin: String, func: (String) -> String): String {
+        return func(origin)
     }
 }
